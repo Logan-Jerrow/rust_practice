6162c5fe314a98ce1fb6a323bb1edb630d6450cd
Logan Jerrow <Logan.Jerrow@Gmail.com>
2022-12-28 19:50:53 -0800

    added square and movment tests

--- chess/base/src/board.rs
+++ chess/base/src/board.rs
@@ -4,30 +4,74 @@ use core::{fmt::Display, slice::Chunks};
 mod movement {
     use crate::piece::Kind;
     use core::str::{self, FromStr};
+    use square::Square;
 
-    #[derive(Debug, Clone, Copy, PartialEq)]
-    enum Square {
-        File(usize),
-        Rank(usize),
-    }
+    pub mod square {
+        use core::str::{self, FromStr};
 
-    impl FromStr for Square {
-        type Err = String;
+        #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+        pub enum Square {
+            File(usize),
+            Rank(usize),
+        }
 
-        fn from_str(s: &str) -> Result<Self, Self::Err> {
-            match s {
-                "a" => Ok(Self::File(0)),
-                "b" => Ok(Self::File(1)),
-                "c" => Ok(Self::File(2)),
-                "d" => Ok(Self::File(3)),
-                "e" => Ok(Self::File(4)),
-                "f" => Ok(Self::File(5)),
-                "g" => Ok(Self::File(6)),
-                "h" => Ok(Self::File(7)),
-                "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" => {
-                    Ok(Self::Rank(s.parse::<usize>().unwrap() - 1))
+        impl FromStr for Square {
+            type Err = String;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                match s {
+                    "a" => Ok(Self::File(0)),
+                    "b" => Ok(Self::File(1)),
+                    "c" => Ok(Self::File(2)),
+                    "d" => Ok(Self::File(3)),
+                    "e" => Ok(Self::File(4)),
+                    "f" => Ok(Self::File(5)),
+                    "g" => Ok(Self::File(6)),
+                    "h" => Ok(Self::File(7)),
+                    "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" => {
+                        Ok(Self::Rank(s.parse::<usize>().unwrap() - 1))
+                    }
+                    _ => Err(format!("Could not parse '{s}'")),
                 }
-                _ => Err(format!("Could not parse {s}.")),
+            }
+        }
+
+        #[cfg(test)]
+        mod tests {
+            use super::*;
+
+            #[test]
+            fn valid_file() {
+                assert_eq!("a".parse::<Square>().unwrap(), Square::File(0));
+                assert_eq!("b".parse::<Square>().unwrap(), Square::File(1));
+                assert_eq!("c".parse::<Square>().unwrap(), Square::File(2));
+                assert_eq!("d".parse::<Square>().unwrap(), Square::File(3));
+                assert_eq!("e".parse::<Square>().unwrap(), Square::File(4));
+                assert_eq!("f".parse::<Square>().unwrap(), Square::File(5));
+                assert_eq!("g".parse::<Square>().unwrap(), Square::File(6));
+                assert_eq!("h".parse::<Square>().unwrap(), Square::File(7));
+            }
+
+            #[test]
+            fn valid_rank() {
+                assert_eq!("1".parse::<Square>().unwrap(), Square::Rank(0));
+                assert_eq!("2".parse::<Square>().unwrap(), Square::Rank(1));
+                assert_eq!("3".parse::<Square>().unwrap(), Square::Rank(2));
+                assert_eq!("4".parse::<Square>().unwrap(), Square::Rank(3));
+                assert_eq!("5".parse::<Square>().unwrap(), Square::Rank(4));
+                assert_eq!("6".parse::<Square>().unwrap(), Square::Rank(5));
+                assert_eq!("7".parse::<Square>().unwrap(), Square::Rank(6));
+                assert_eq!("8".parse::<Square>().unwrap(), Square::Rank(7));
+            }
+
+            #[test]
+            fn invalid_sqaure() {
+                assert!("i".parse::<Square>().is_err());
+                assert!("\n".parse::<Square>().is_err());
+                assert!("!".parse::<Square>().is_err());
+                assert!("9".parse::<Square>().is_err());
+                assert!("0".parse::<Square>().is_err());
+                assert!("00324".parse::<Square>().is_err());
             }
         }
     }
@@ -92,11 +136,52 @@ mod movement {
         use super::*;
 
         #[test]
-        fn parse_mov() {
-            let mov = "Qg5".parse::<Movement>().unwrap();
-            assert_eq!(mov.piece, Kind::Queen);
-            assert_eq!(mov.file, Square::File(6));
-            assert_eq!(mov.rank, Square::Rank(4));
+        fn parse_notation_len2() {
+            assert_eq!(
+                "g5".parse::<Movement>(),
+                Ok(Movement {
+                    piece: Kind::Pawn,
+                    ambiguitie: None,
+                    file: Square::File(6),
+                    rank: Square::Rank(4)
+                })
+            );
+        }
+
+        #[test]
+        fn parse_notation_len3() {
+            assert_eq!(
+                "Qg5".parse::<Movement>(),
+                Ok(Movement {
+                    piece: Kind::Queen,
+                    ambiguitie: None,
+                    file: Square::File(6),
+                    rank: Square::Rank(4)
+                })
+            );
+        }
+
+        #[test]
+        fn parse_notation_len4() {
+            assert_eq!(
+                "B8g5".parse::<Movement>(),
+                Ok(Movement {
+                    piece: Kind::Bishop,
+                    ambiguitie: Some(Square::Rank(7)),
+                    file: Square::File(6),
+                    rank: Square::Rank(4)
+                })
+            );
+
+            assert_eq!(
+                "Bhg5".parse::<Movement>(),
+                Ok(Movement {
+                    piece: Kind::Bishop,
+                    ambiguitie: Some(Square::File(7)),
+                    file: Square::File(6),
+                    rank: Square::Rank(4)
+                })
+            );
         }
     }
 }
@@ -151,7 +236,6 @@ macro_rules! board {
     [$($piece:tt) *] => {
         Board([
         $(
-            // dbg!($piece);
             piece!($piece),
         )*
         ])
